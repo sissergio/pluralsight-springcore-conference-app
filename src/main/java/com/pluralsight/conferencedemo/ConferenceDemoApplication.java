a14eac3a32a99314c6af5646f41e167593cd22db
Sergio D. Pardo <sis_sergio@hotmail.com>
2020-05-10 18:52:44 -0400

    added and commented the proper way to build war file

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConferenceDemoApplication {
+public class ConferenceDemoApplication /*extends SpringBootServletInitializer*/ {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConferenceDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceDemoApplication.class, args);
+    }
 
 }
--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -1,10 +1,6 @@
 package com.pluralsight.conferencedemo.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
